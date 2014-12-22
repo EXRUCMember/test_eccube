931235d66cf63a47a41b05b15830fc77a8365696
oudomseilaphok <oudomseilaphok@gmail.com>
2014-12-22 11:58:14 +0700

    add page

--- data/Smarty/templates_c/default_en-US/%%4D^4DA^4DA49DF0%%testpage.tpl.php
+++ data/Smarty/templates_c/default_en-US/%%4D^4DA^4DA49DF0%%testpage.tpl.php
@@ -1,2 +1,3 @@
-<?php /* Smarty version 2.6.26, created on 2014-12-22 04:55:33
+<?php /* Smarty version 2.6.26, created on 2014-12-22 04:57:35
          compiled from C:%5Cwamp%5Cwww%5Ctest_eccube%5Chtml/../data/Smarty/templates/default_en-US/user_data/testpage.tpl */ ?>
+<p>Hi Chiva</p>
