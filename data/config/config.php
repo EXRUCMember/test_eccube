d86517d8cf2a1fc1e9b941824eccf364ae1b5b28
oudomseilaphok <oudomseilaphok@gmail.com>
2014-12-22 08:45:51 +0700

    seila installations 2

---
+++ data/config/config.php
@@ -0,0 +1,22 @@
+<?php
+define('ECCUBE_INSTALL', 'ON');
+define('HTTP_URL', 'http://localhost:8585/test_eccube/html/');
+define('HTTPS_URL', 'http://localhost:8585/test_eccube/html/');
+define('ROOT_URLPATH', '/test_eccube/html/');
+define('DOMAIN_NAME', '');
+define('DB_TYPE', 'mysql');
+define('DB_USER', 'ectest');
+define('DB_PASSWORD', 'ectest');
+define('DB_SERVER', '127.0.0.1');
+define('DB_NAME', 'ectest');
+define('DB_PORT', '');
+define('ADMIN_DIR', 'admin/');
+define('ADMIN_FORCE_SSL', FALSE);
+define('ADMIN_ALLOW_HOSTS', 'a:0:{}');
+define('AUTH_MAGIC', 'dreawiarougecriodeviatriahoumioletrastut');
+define('PASSWORD_HASH_ALGOS', 'sha256');
+define('MAIL_BACKEND', 'mail');
+define('SMTP_HOST', '');
+define('SMTP_PORT', '');
+define('SMTP_USER', '');
+define('SMTP_PASSWORD', '');
