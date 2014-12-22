53e5e84ea0186a74b755d4dbed46934dd95ae472
oudomseilaphok <oudomseilaphok@gmail.com>
2014-12-22 09:06:17 +0700

    test

---
+++ data/Smarty/templates_c/admin/%%36^36B^36B31128%%admin_popup_footer.tpl.php
@@ -0,0 +1,6 @@
+<?php /* Smarty version 2.6.26, created on 2014-12-22 02:04:56
+         compiled from C:%5Cwamp%5Cwww%5Ctest_eccube%5Chtml/../data/Smarty/templates/admin/admin_popup_footer.tpl */ ?>
+
+</div>
+</body>
+</html>
